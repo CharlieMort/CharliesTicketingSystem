e0a1599f5c8349a63a4e7fec7130aa350e67affd
CharlieMort <charlie_mort@outlook.com>
2025-07-13 00:47:38 +0100

    page

--- backend/main.go
+++ backend/main.go
@@ -112,7 +112,7 @@ func main() {
 	router.POST("/api/tickets/create", createTicket)
 	router.PUT("/api/tickets/update/:id", updateTicket)
 	router.DELETE("/api/tickets/delete/:id", deleteTicket)
-	router.Static("/", "../frontend/build")
+	router.Static("/page/", "../frontend/build")
 	router.Run("localhost:8080")
 
 	//fmt.Println(GetRecordByProperty("tickets", "title", "cum"))
