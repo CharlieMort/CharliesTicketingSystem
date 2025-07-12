29cfc888134bf70063a62024a842d4f6b1ccc473
CharlieMort <charlie_mort@outlook.com>
2025-07-13 00:36:34 +0100

    show dir

--- backend/main.go
+++ backend/main.go
@@ -73,6 +73,14 @@ func deleteTicket(c *gin.Context) {
 
 func main() {
 	fmt.Println("Hello Ticket_API")
+	entries, err := os.ReadDir("./")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, e := range entries {
+		fmt.Println(e.Name())
+	}
 	err := godotenv.Load(fmt.Sprintf("./.env.%s", ENV))
 	if err != nil {
 		log.Fatal("Could Not Load .env file for env:", ENV)
